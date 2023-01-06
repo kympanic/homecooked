55bac380fd01ec548c335d33b5c1fabc73524917
Alex Hiller <asonnyhiller@gmail.com>
2023-01-06 00:55:28 -0500

    it turns out that nothing works

--- app/api/payment_routes.py
+++ app/api/payment_routes.py
@@ -1,29 +1,37 @@
 from flask import Blueprint, request
 from ..models import Payment, db
 from ..utils import Print
-from flask_login import current_user
+from flask_login import current_user, login_required
+import datetime
+
 
 payment_routes = Blueprint('payments', __name__)
 
 @payment_routes.route('/<int:id>')
+@login_required
 def get_paymentinfo_by_id(id):
     paymentInfo = Payment.query.get(id)
     
     return paymentInfo.to_dict()
 
 @payment_routes.route('', methods=["POST"])
+@login_required
 def create_paymentinfo():
     paymentInfo = request.json
     
     # ** is Python's spread ('...' in JS)
-    new_paymentInfo = Payment(**paymentInfo, user_id=current_user.id)
+    #take a string from expiration date and turn it into a datetime object here
+    new_paymentInfo = Payment(**paymentInfo, expiration=datetime.date(2023,2,1), user_id=current_user.id)
     
     db.session.add(new_paymentInfo)
     db.session.commit()
     
+    
     return {new_paymentInfo.id: new_paymentInfo.to_dict()}
+    
 
 @payment_routes.route('/<int:id>', methods=["PATCH", "PUT"])
+@login_required
 def edit_paymentinfo():
     payment_data = request.json
     
@@ -36,11 +44,12 @@ def edit_paymentinfo():
     return {payment_info.id: payment_info.to_dict()}
 
 @payment_routes.route('/<int:id>', methods=["DELETE"])
+@login_required
 def delete_paymentinfo(id):
     data = request.json
 
     if data["user_id"] != current_user.id:
-        return {"error": "You are not authorized to delete this tweet"}, 401
+        return {"error": "You are not authorized to delete this payment information"}, 401
     
     payment_info = Payment.query.get(id)
     
--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, request
 from flask_login import login_required
 from app.models import User, Order
 from flask_login import current_user
+from ..utils import Print
 
 user_routes = Blueprint('users', __name__)
 
@@ -26,14 +27,15 @@ def user(id):
     return user.to_dict()
 
 @user_routes.route('/<int:id>/orders')
+@login_required
 def get_all_orders_by_specific_user(id):
     if id != current_user.id:
         return {"error": "You are not authorized to view this information"}, 401
     orders = Order.query.all()
-    users_orders = []
+    users_orders = {}
     
     for order in orders:
         if order.user_id == id:
-            users_orders.append(order)
-    print(users_orders)
-    return {users_orders}
+            users_orders.update({order.id: order})
+    Print(users_orders)
+    return users_orders
