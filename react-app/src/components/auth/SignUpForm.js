101ca9e6cdf746f0d6b25acec49db736e9c2288f
Daniel Yoo <dacapo0509@gmail.com>
2023-01-06 09:23:50 -0800

    edited signup form to have the valid fields. edited signup for backend signup_form. Successfully created user

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,94 +1,169 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router-dom';
-import { signUp } from '../../store/session';
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { signUp } from "../../store/session";
 
 const SignUpForm = () => {
-  const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-  const user = useSelector(state => state.session.user);
-  const dispatch = useDispatch();
+	const [errors, setErrors] = useState([]);
+	const [username, setUsername] = useState("");
+	const [email, setEmail] = useState("");
+	const [shopName, setShopName] = useState("");
+	const [profileImg, setProfileImg] = useState("");
+	const [phoneNumber, setPhoneNumber] = useState("");
+	const [zipcode, setZipcode] = useState("");
+	const [password, setPassword] = useState("");
+	const [repeatPassword, setRepeatPassword] = useState("");
+	const user = useSelector((state) => state.session.user);
+	const dispatch = useDispatch();
 
-  const onSignUp = async (e) => {
-    e.preventDefault();
-    if (password === repeatPassword) {
-      const data = await dispatch(signUp(username, email, password));
-      if (data) {
-        setErrors(data)
-      }
-    }
-  };
+	const onSignUp = async (e) => {
+		e.preventDefault();
+		const newUser = {
+			username,
+			email,
+			password,
+			shop_name: shopName,
+			profile_img: profileImg,
+			phone_number: phoneNumber,
+			zipcode,
+		};
+		if (password === repeatPassword) {
+			const data = await dispatch(
+				signUp(newUser)
+				// signUp(
+				// 	username,
+				// 	email,
+				// 	password,
+				// 	shopName,
+				// 	profileImg,
+				// 	phoneNumber,
+				// 	zipcode
+				// )
+			);
+			if (data) {
+				setErrors(data);
+			}
+		}
+	};
 
-  const updateUsername = (e) => {
-    setUsername(e.target.value);
-  };
+	const updateUsername = (e) => {
+		setUsername(e.target.value);
+	};
 
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
-  };
+	const updateEmail = (e) => {
+		setEmail(e.target.value);
+	};
 
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
+	const updateShopName = (e) => {
+		setShopName(e.target.value);
+	};
 
-  const updateRepeatPassword = (e) => {
-    setRepeatPassword(e.target.value);
-  };
+	const updateProfileImg = (e) => {
+		setProfileImg(e.target.value);
+	};
 
-  if (user) {
-    return <Redirect to='/' />;
-  }
+	const updatePhoneNumber = (e) => {
+		setPhoneNumber(e.target.value);
+	};
 
-  return (
-    <form onSubmit={onSignUp}>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div>
-        <label>User Name</label>
-        <input
-          type='text'
-          name='username'
-          onChange={updateUsername}
-          value={username}
-        ></input>
-      </div>
-      <div>
-        <label>Email</label>
-        <input
-          type='text'
-          name='email'
-          onChange={updateEmail}
-          value={email}
-        ></input>
-      </div>
-      <div>
-        <label>Password</label>
-        <input
-          type='password'
-          name='password'
-          onChange={updatePassword}
-          value={password}
-        ></input>
-      </div>
-      <div>
-        <label>Repeat Password</label>
-        <input
-          type='password'
-          name='repeat_password'
-          onChange={updateRepeatPassword}
-          value={repeatPassword}
-          required={true}
-        ></input>
-      </div>
-      <button type='submit'>Sign Up</button>
-    </form>
-  );
+	const updateZipcode = (e) => {
+		setZipcode(e.target.value);
+	};
+	const updatePassword = (e) => {
+		setPassword(e.target.value);
+	};
+
+	const updateRepeatPassword = (e) => {
+		setRepeatPassword(e.target.value);
+	};
+
+	if (user) {
+		return <Redirect to="/" />;
+	}
+
+	return (
+		<form onSubmit={onSignUp}>
+			<div>
+				{errors.map((error, ind) => (
+					<div key={ind}>{error}</div>
+				))}
+			</div>
+			<div>
+				<label>User Name</label>
+				<input
+					type="text"
+					name="username"
+					onChange={updateUsername}
+					value={username}
+				></input>
+			</div>
+			<div>
+				<label>Email</label>
+				<input
+					type="text"
+					name="email"
+					onChange={updateEmail}
+					value={email}
+				></input>
+			</div>
+			<div>
+				<label>Shop Name</label>
+				<input
+					type="text"
+					name="shopName"
+					onChange={updateShopName}
+					value={shopName}
+				></input>
+			</div>
+			<div>
+				<label>Profile Image</label>
+				<input
+					type="txt"
+					name="profileImg"
+					onChange={updateProfileImg}
+					value={profileImg}
+				></input>
+			</div>
+			<div>
+				<label>Phone Number</label>
+				<input
+					type="text"
+					name="phoneNumber"
+					onChange={updatePhoneNumber}
+					value={phoneNumber}
+				></input>
+			</div>
+			<div>
+				<label>Zipcode</label>
+				<input
+					type="text"
+					name="zipcode"
+					onChange={updateZipcode}
+					value={zipcode}
+				></input>
+			</div>
+			<div>
+				<label>Password</label>
+				<input
+					type="password"
+					name="password"
+					onChange={updatePassword}
+					value={password}
+				></input>
+			</div>
+			<div>
+				<label>Repeat Password</label>
+				<input
+					type="password"
+					name="repeat_password"
+					onChange={updateRepeatPassword}
+					value={repeatPassword}
+					required={true}
+				></input>
+			</div>
+			<button type="submit">Sign Up</button>
+		</form>
+	);
 };
 
 export default SignUpForm;
