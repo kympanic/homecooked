a428d029c0e3051f45df35c3e2ac5c1f67e31ba4
Alex Hiller <asonnyhiller@gmail.com>
2023-01-11 20:20:52 -0500

    made it so that the cart page calculates total number of items in cart and total price of order. also added a counter to the navbar to show how many items are in the cart. nothing is pretty but it works and that's what I care about <3

--- react-app/src/components/CartPage/index.jsx
+++ react-app/src/components/CartPage/index.jsx
@@ -1,17 +1,32 @@
 import CartItem from "./cartItem";
-import { useDispatch, useSelector} from 'react-redux'
-import { useState } from "react";
+import { useSelector} from 'react-redux'
+import { useState, useEffect } from "react";
 
 import { getAllCartItems } from "../../store/session";
 
 const CartPage = () => {
-	const dispatch = useDispatch();
 	const cartItems = useSelector(getAllCartItems)
 	const products = useSelector((state) => state.products)
 
-	const totalItems = () => {
-		return;
-	}
+	//hmmm. totalItems and totalPrice need to update whenever the count of any item changes
+	//so we need these to be in a useEffect!
+	//and it'll probably be easiest to handle these values as part of state. ok.
+	const [ totalItems, setTotalItems ] = useState(0);
+	const [ totalPrice, setTotalPrice ] = useState(0);
+
+	useEffect(() => {
+		let itemCount = 0;
+		let price = 0;
+
+		//gotta set the quantity of items and the price to zero
+		//iterate over the cart
+		cartItems.forEach(item => {
+			itemCount += item.count
+			price += (products[item.id].price * item.count)
+		});
+		setTotalItems(itemCount);
+		setTotalPrice(price);
+	}, [cartItems, totalItems, totalPrice])
 
 	return (
 		<div>
@@ -28,8 +43,8 @@ const CartPage = () => {
 			</div>
 			<div>
 				<h2>This takes us to the checkout form</h2>
-				<span>Total Items: Number</span>
-				<span>Total Price: $Number</span>
+				<span>Total Items: {totalItems}</span>
+				<span>Total Price: ${(Math.round(totalPrice * 100) / 100).toFixed(2)}</span>
 				<button>Proceed To Checkout</button>
 				
 			</div>
--- react-app/src/components/NavBar/index.jsx
+++ react-app/src/components/NavBar/index.jsx
@@ -1,8 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import LogoutButton from "../auth/LogoutButton";
+import { useState, useEffect } from "react";
+import { useSelector} from 'react-redux'
+
+import { getAllCartItems } from "../../store/session";
 
 const NavBar = () => {
+	const cartItems = useSelector(getAllCartItems)
+
+	const [ totalItems, setTotalItems ] = useState(0);
+
+	useEffect(() => {
+		let itemCount = 0;
+		cartItems.forEach(item => {
+			itemCount += item.count
+		});
+		setTotalItems(itemCount);
+	}, [cartItems, totalItems])
 	return (
 		<nav>
 			<ul>
@@ -32,6 +47,7 @@ const NavBar = () => {
 				<li>
 					<NavLink to ="/cart" exact={true}>
 						Cart
+						<div className="cartCounter">{totalItems}</div>
 					</NavLink>
 				</li>
 			</ul>
