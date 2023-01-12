113eee727c4cfc708c95339bb3b62c92904c6dd5
Alex Hiller <asonnyhiller@gmail.com>
2023-01-12 01:21:41 -0500

    stop being frustrating

--- react-app/src/components/ProfilePage/UserAvgRating.jsx
+++ react-app/src/components/ProfilePage/UserAvgRating.jsx
@@ -2,8 +2,8 @@ const UserAvgRating = ({user, products}) => {
     const selectedProducts = products?.filter((product) => {
 		return product?.userId === parseInt(user.id);
     });
-    const reviewArr = selectedProducts.map((el) => Number(el.avgRating))
-    const reviewAvg = reviewArr.reduce((a, b) => a + b) / reviewArr.length
+    const reviewArr = selectedProducts?.map((el) => Number(el.avgRating))
+    const reviewAvg = reviewArr?.reduce((a, b) => a + b) / reviewArr.length
     return (
         <span>{reviewAvg}</span>
     )
--- react-app/src/components/ProfilePage/UserReviews.jsx
+++ react-app/src/components/ProfilePage/UserReviews.jsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import ModalEditReview from "../Modals/EditReview/ModalEditReview";
+import ModalDeleteReview from "../Modals/DeleteReview/ModalDeleteReview";
 
 const UserReviews = ({user}) => {
 	
