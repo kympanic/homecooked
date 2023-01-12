ed4facf4dad662714fef0e5aebfa730894fd898b
Alex Hiller <asonnyhiller@gmail.com>
2023-01-12 01:04:23 -0500

    turned the review average process into its own component

--- react-app/src/components/ProfilePage/ShopOwnerInfo.jsx
+++ react-app/src/components/ProfilePage/ShopOwnerInfo.jsx
@@ -1,4 +1,5 @@
 import { useSelector } from "react-redux";
+import UserAvgRating from "./UserAvgRating";
 
 const ShopOwnerInfo = ({user}) => {
     const products = useSelector((state) => Object.values(state.products));
@@ -29,7 +30,7 @@ const ShopOwnerInfo = ({user}) => {
             <div>{user?.zipcode}
                 <p>Calculated location based on zip code</p></div>
             <div>Owner of {user?.shopName}</div>
-            <div>Rating: {reviewAvg}({reviewArr.length}), "{reviewCommenter()}"</div>
+            <div>Rating: <UserAvgRating user={user} products={products}/>{' '}(From {reviewArr.length} Reviews),{' '}"{reviewCommenter()}"</div>
         </div>
     )
 }
---
+++ react-app/src/components/ProfilePage/UserAvgRating.jsx
@@ -0,0 +1,12 @@
+const UserAvgRating = ({user, products}) => {
+    const selectedProducts = products?.filter((product) => {
+		return product?.userId === parseInt(user.id);
+    });
+    const reviewArr = selectedProducts.map((el) => Number(el.avgRating))
+    const reviewAvg = reviewArr.reduce((a, b) => a + b) / reviewArr.length
+    return (
+        <span>{reviewAvg}</span>
+    )
+}
+
+export default UserAvgRating
