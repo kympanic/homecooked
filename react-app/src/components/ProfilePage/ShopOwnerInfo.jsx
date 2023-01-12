94007f7735d7fea85e50d8df2a781c94d37b3de9
Alex Hiller <asonnyhiller@gmail.com>
2023-01-12 00:53:40 -0500

    created user profile page and most of the related components

--- react-app/src/components/ProfilePage/ShopOwnerInfo.jsx
+++ react-app/src/components/ProfilePage/ShopOwnerInfo.jsx
@@ -3,19 +3,33 @@ import { useSelector } from "react-redux";
 const ShopOwnerInfo = ({user}) => {
     const products = useSelector((state) => Object.values(state.products));
 
-    console.log(products, "all products")
-    console.log(user.products, "user products")
+
     const selectedProducts = products?.filter((product) => {
 		return product?.userId === parseInt(user.id);
-	});
-    console.log(selectedProducts)
+    });
+    const reviewArr = selectedProducts.map((el) => Number(el.avgRating))
+    const reviewAvg = reviewArr.reduce((a, b) => a + b) / reviewArr.length
     
+    const reviewCommenter = () => {
+        if (reviewAvg < 2) {
+            return "Overwhelmingly Negative"
+        } else if (reviewAvg >= 2 && reviewAvg < 3) {
+            return "Negative"
+        } else if (reviewAvg >= 3 && reviewAvg <= 3.5) {
+            return "Mixed"
+        } else if (reviewAvg > 3.5 && reviewAvg < 4.5) {
+            return "Positive"
+        } else if (reviewAvg > 4.5) {
+            return "Overwhelmingly Positive"
+        }
+    }
+
     return (
         <div>
             <div>{user?.zipcode}
                 <p>Calculated location based on zip code</p></div>
             <div>Owner of {user?.shopName}</div>
-            <div>Amalgamate Review Average. (Number of Reviews) "Commentary on average"</div>
+            <div>Rating: {reviewAvg}({reviewArr.length}), "{reviewCommenter()}"</div>
         </div>
     )
 }
--- react-app/src/components/ProfilePage/index.jsx
+++ react-app/src/components/ProfilePage/index.jsx
@@ -1,12 +1,61 @@
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import UserInfo from "./UserInfo";
+import ShopOwnerInfo from "./ShopOwnerInfo";
+import UserReviews from "./UserReviews";
 
+//note that this page is a public page!
+//the nav bar button that links to it should get which page it sends you to thru session
+//but!!!!!!!
+//other users' profiles should be viewable by clicking on their usernames on reviews or on their stores
 
 const ProfilePage = () => {
+
+	const sessionUserId = useSelector((state) => state?.session.user.id);
+	const { userId } = useParams()
+	const user = useSelector((state) => state?.users[userId])
+	console.log(user)
+
 
 
 	return (
 		<div>
 			<h1>Profile Page</h1>
-
+			<div> 
+				<UserInfo user={user} />
+			</div>
+			{ user?.shopName ? (
+			<div> 
+				<ShopOwnerInfo user={user} />
+			</div>
+			) : null
+			}
+			{ user?.id === sessionUserId ? (
+				<div>
+					<button>Edit Public Profile</button>
+					<div> Note that this is the only means to edit profile image (when have you ever put in a profile image on sign up?)</div>
+					<button>Change Password</button>
+					<div> Note that this is the only way to change your password.</div>
+				</div>
+				) : null
+			}
+			{user?.shopName ? (
+				<div> 
+					<Link to={`/store/${userId}`}>
+						Click here to go to "{user?.shopName}"
+					</Link>
+				</div>
+			) : ( user?.id === sessionUserId ?(
+				<div> 
+					<button>Become a Vendor</button>
+					<div> Note that this and a nav bar button are the only way to access this form. </div>
+				</div>
+				) : null
+			)}
+			<div> 
+				<UserReviews user={user} />
+			</div>
 		</div>
 	);
 };
