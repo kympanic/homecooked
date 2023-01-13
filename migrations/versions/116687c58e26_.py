1c6a9bac7490d71e5013fa8d0bc8d7c4b6efb60d
Daniel Yoo <dacapo0509@gmail.com>
2023-01-12 23:54:24 -0800

    reseeded and remigrated db

--- migrations/versions/116687c58e26_.py
+++ migrations/versions/116687c58e26_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 66280a0d154b
+Revision ID: 116687c58e26
 Revises: 
-Create Date: 2023-01-12 23:52:33.840897
+Create Date: 2023-01-12 23:54:06.032931
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '66280a0d154b'
+revision = '116687c58e26'
 down_revision = None
 branch_labels = None
 depends_on = None
