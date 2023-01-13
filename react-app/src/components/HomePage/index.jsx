ae302076ac4db00420dcdf802ff48cb5a3e6b43f
Alex Hiller <asonnyhiller@gmail.com>
2023-01-12 20:52:17 -0500

    made it so that the home page doesn't crash if a user is not logged in, but instead displays welcome to homecooked! and asks them to log in or sign up.

--- react-app/src/components/HomePage/index.jsx
+++ react-app/src/components/HomePage/index.jsx
@@ -11,71 +11,84 @@ const zipCodeData = require("zipcode-city-distance");
 const HomePage = () => {
 	const dispatch = useDispatch();
 	const history = useHistory();
-	const sessionUserZipcode = useSelector(
-		(state) => state.session.user.zipcode
+	const sessionUser = useSelector(
+		(state) => state.session.user
 	);
+	let sessionUserZipcode;
+	if(sessionUser) {
+		sessionUserZipcode =  sessionUser.zipcode
+	}
+	console.log(sessionUserZipcode)
 	const allStoresArray = useSelector((state) => Object.values(state.users));
 
 	useEffect(() => {
 		dispatch(getAllUsersThunk());
 	}, [dispatch]);
-
+	console.log(sessionUser, "test")
 	return (
 		<>
-			{" "}
-			<hr></hr>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div className="stores-container">
-				{allStoresArray.map((store) =>
-					store && store.id && store.shopName ? (
-						<div key={store?.id}>
-							<div className="store-details">
-								<img
-									id="shop-splash-img"
-									src={store?.shopSplashImg}
-									alt="vendor-splash-img"
-								/>
-							</div>
-							<Link
-								className="store-link"
-								to={`/store/${store.id}`}
-							>
-								{store.shopName}
-							</Link>
-							<div className="secondary-text">
-								Average Rating:
-								<FontAwesomeIcon
-									className="star"
-									icon={faStar}
-								/>
-							</div>
+			{sessionUser ? (
+			<div>
+				{" "}
+				<hr></hr>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div></div>
+				<div className="stores-container">
+					{allStoresArray.map((store) =>
+						store && store.id && store.shopName ? (
+							<div key={store?.id}>
+								<div className="store-details">
+									<img
+										id="shop-splash-img"
+										src={store?.shopSplashImg}
+										alt="vendor-splash-img"
+									/>
+								</div>
+								<Link
+									className="store-link"
+									to={`/store/${store.id}`}
+								>
+									{store.shopName}
+								</Link>
+								<div className="secondary-text">
+									Average Rating:
+									<FontAwesomeIcon
+										className="star"
+										icon={faStar}
+									/>
+								</div>
 
-							<div className="secondary-text">
-								Zipcode: {store.zipcode}
+								<div className="secondary-text">
+									Zipcode: {store.zipcode}
+								</div>
+								<div className="secondary-text">
+									Distance:{" "}
+									{zipCodeData
+										.zipCodeDistance(
+											sessionUserZipcode,
+											store.zipcode,
+											"M"
+										)
+										.toFixed(2)}{" "}
+									miles
+								</div>
+								<div>
+									<br></br>
+								</div>
 							</div>
-							<div className="secondary-text">
-								Distance:{" "}
-								{zipCodeData
-									.zipCodeDistance(
-										sessionUserZipcode,
-										store.zipcode,
-										"M"
-									)
-									.toFixed(2)}{" "}
-								miles
-							</div>
-							<div>
-								<br></br>
-							</div>
-						</div>
-					) : (
-						<div></div>
-					)
-				)}
+						) : (
+							<div></div>
+						)
+					)}
+				</div>
 			</div>
+			) : (
+				<div>
+					<p>Welcome to HomeCooked! Please Sign Up or Log In to continue!</p>
+				</div>
+			)}
 		</>
 	);
 };
