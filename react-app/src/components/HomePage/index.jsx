be77b94fc0683c82c6fcbc4323abde9e80c2bf32
Daniel Yoo <dacapo0509@gmail.com>
2023-01-12 17:56:23 -0800

    merge

--- react-app/src/components/HomePage/index.jsx
+++ react-app/src/components/HomePage/index.jsx
@@ -5,88 +5,90 @@ import { useDispatch, useSelector } from "react-redux";
 import { getAllUsersThunk } from "../../store/users";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
+import { useHistory } from "react-router-dom";
 import "./HomePage.css";
 const zipCodeData = require("zipcode-city-distance");
 
 const HomePage = () => {
 	const dispatch = useDispatch();
 	const history = useHistory();
-	const sessionUser = useSelector(
-		(state) => state.session.user
-	);
+	const sessionUser = useSelector((state) => state.session.user);
 	let sessionUserZipcode;
-	if(sessionUser) {
-		sessionUserZipcode =  sessionUser.zipcode
+	if (sessionUser) {
+		sessionUserZipcode = sessionUser.zipcode;
 	}
-	console.log(sessionUserZipcode)
+	console.log(sessionUserZipcode);
 	const allStoresArray = useSelector((state) => Object.values(state.users));
 
 	useEffect(() => {
 		dispatch(getAllUsersThunk());
 	}, [dispatch]);
-	console.log(sessionUser, "test")
+	console.log(sessionUser, "test");
 	return (
 		<>
 			{sessionUser ? (
-			<div>
-				{" "}
-				<hr></hr>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div></div>
-				<div className="stores-container">
-					{allStoresArray.map((store) =>
-						store && store.id && store.shopName ? (
-							<div key={store?.id}>
-								<div className="store-details">
-									<img
-										id="shop-splash-img"
-										src={store?.shopSplashImg}
-										alt="vendor-splash-img"
-									/>
-								</div>
-								<Link
-									className="store-link"
-									to={`/store/${store.id}`}
-								>
-									{store.shopName}
-								</Link>
-								<div className="secondary-text">
-									Average Rating:
-									<FontAwesomeIcon
-										className="star"
-										icon={faStar}
-									/>
-								</div>
+				<div>
+					{" "}
+					<hr></hr>
+					<div></div>
+					<div></div>
+					<div></div>
+					<div></div>
+					<div className="stores-container">
+						{allStoresArray.map((store) =>
+							store && store.id && store.shopName ? (
+								<div key={store?.id}>
+									<div className="store-details">
+										<img
+											id="shop-splash-img"
+											src={store?.shopSplashImg}
+											alt="vendor-splash-img"
+										/>
+									</div>
+									<Link
+										className="store-link"
+										to={`/store/${store.id}`}
+									>
+										{store.shopName}
+									</Link>
+									<div className="secondary-text">
+										Average Rating:
+										<FontAwesomeIcon
+											className="star"
+											icon={faStar}
+										/>
+									</div>
 
-								<div className="secondary-text">
-									Zipcode: {store.zipcode}
+									<div className="secondary-text">
+										Zipcode: {store.zipcode}
+									</div>
+									<div className="secondary-text">
+										Distance:{" "}
+										{zipCodeData
+											.zipCodeDistance(
+												sessionUserZipcode,
+												store.zipcode,
+												"M"
+											)
+											.toFixed(2)}{" "}
+										miles
+									</div>
+									<div>
+										<br></br>
+									</div>
 								</div>
-								<div className="secondary-text">
-									Distance:{" "}
-									{zipCodeData
-										.zipCodeDistance(
-											sessionUserZipcode,
-											store.zipcode,
-											"M"
-										)
-										.toFixed(2)}{" "}
-									miles
-								</div>
-								<div>
-									<br></br>
-								</div>
-							</div>
-						) : (
-							<div></div>
-						)
-					)}
+							) : (
+								<div></div>
+							)
+						)}
+					</div>
 				</div>
-			</div>
 			) : (
 				<div>
-					<p>Welcome to HomeCooked! Please Sign Up or Log In to continue!</p>
+					<p>
+						Welcome to HomeCooked! Please Sign Up or Log In to
+						continue!
+					</p>
 				</div>
 			)}
 		</>
