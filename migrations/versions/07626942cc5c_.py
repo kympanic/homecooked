196eaea46691f54843ccb5d72a02737cdf3cf806
Daniel Yoo <dacapo0509@gmail.com>
2023-01-13 01:39:39 -0800

    did revision for '07626942cc5c'

---
+++ migrations/versions/07626942cc5c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 07626942cc5c
+Revises: 35f1c3120348
+Create Date: 2023-01-13 01:38:23.285369
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '07626942cc5c'
+down_revision = '35f1c3120348'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
