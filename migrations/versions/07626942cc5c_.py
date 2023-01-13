8c3a6bb992489aba47e94d01b5033bf9db0ef408
Daniel Yoo <dacapo0509@gmail.com>
2023-01-13 01:13:09 -0800

    flask db revision --rev-id faf3ebfbe667 and flask db migrate and upgrade to try to fix alembic issue

---
+++ migrations/versions/07626942cc5c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 07626942cc5c
+Revises: 8bdeecabac42
+Create Date: 2023-01-13 01:11:29.615587
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '07626942cc5c'
+down_revision = '8bdeecabac42'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- migrations/versions/8bdeecabac42_.py
+++ migrations/versions/8bdeecabac42_.py
@@ -8,7 +8,9 @@ Create Date: 2023-01-13 00:30:33.878606
 from alembic import op
 import sqlalchemy as sa
 
-
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 # revision identifiers, used by Alembic.
 revision = '8bdeecabac42'
 down_revision = None
@@ -83,6 +85,15 @@ def upgrade():
     sa.ForeignKeyConstraint(['products'], ['products.id'], ),
     sa.PrimaryKeyConstraint('products', 'orders')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE payments SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE products SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE orders SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE reviews SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE product_orders SET SCHEMA {SCHEMA};")
+
+
     # ### end Alembic commands ###
 
 
