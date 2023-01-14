98df521c474a5fee364a61d87f3a267d820689ba
Megha Sahgal <meghasahgal@Meghas-Air.localdomain>
2023-01-14 13:03:59 -0800

    added footer

--- react-app/src/components/Footer/index.jsx
+++ react-app/src/components/Footer/index.jsx
@@ -1,10 +1,14 @@
+import {Link} from 'react-router-dom'
 import "./footerpage.css";
 
 const Footer = () => {
 	return (
 		<div className="footer-page-container">
-			<h1>this is the footer</h1>
-		</div>
+			<a href='https://github.com/kympanic/homecooked'>
+        <h1 className="about">  © 2023 HomeCooked, Inc. | About </h1>
+        </a>
+
+        </div>
 	);
 };
 
