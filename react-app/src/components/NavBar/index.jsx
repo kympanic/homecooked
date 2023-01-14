33b7cea386ef50fbcba8e116c9745558f931eea6
Alex Hiller <asonnyhiller@gmail.com>
2023-01-14 15:25:23 -0500

    navbar clean up

--- react-app/src/components/NavBar/index.jsx
+++ react-app/src/components/NavBar/index.jsx
@@ -7,9 +7,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faShoppingCart, faSearch} from "@fortawesome/free-solid-svg-icons";
 import { getAllCartItems } from "../../store/session";
 import { getAllUsersThunk } from "../../store/users";
+import logo from "./icon_logo_draft.png"
 
 const NavBar = () => {
 	const cartItems = useSelector(getAllCartItems)
+	const sessionUser = useSelector((state) => state.session.user);
+
 
 	const [ totalItems, setTotalItems ] = useState(0);
 
@@ -24,32 +27,47 @@ const NavBar = () => {
 	const stores = useSelector(getAllUsersThunk)
 	return (
 		<nav>
-			<ul>
-				<li>
+			<ul className="navbar">
+				<li className="barLink">
 					<NavLink to="/" exact={true} activeClassName="active">
+						<img 
+							src={logo}
+							alt="HomeCooked Logo"
+						/>
 						Home
 					</NavLink>
 				</li>
+				{sessionUser ? (
+					<>
+						<li className="barLink">
+							<LogoutButton />
+						</li>
+						<li className="barLink">
+							<NavLink to={`/users/${sessionUser.id}`} exact={true}>
+								{sessionUser.username}
+							</NavLink>
+						</li>
+					</>
+				) : (
+					<>
+						<li className="barLink">
+							<NavLink to="/login" exact={true} activeClassName="active">
+								Login
+							</NavLink>
+						</li>
+						<li className="barLink">
+							<NavLink
+								to="/sign-up"
+								exact={true}
+								activeClassName="active"
+							>
+								Sign Up
+							</NavLink>
+						</li>
+					</>	
+				)}
 
 
-				<li>
-					<NavLink to="/login" exact={true} activeClassName="active">
-						Login
-					</NavLink>
-				</li>
-				<li>
-					<NavLink
-						to="/sign-up"
-						exact={true}
-						activeClassName="active"
-					>
-						Sign Up
-					</NavLink>
-				</li>
-
-				<li>
-					<LogoutButton />
-				</li>
 				<li>
 					<NavLink to ="/cart" exact={true}>
 					<FontAwesomeIcon className="shopping" icon={faShoppingCart} />
