dca7d6166c9c7b19a71fcaef5356160326df83ba
Megha Sahgal <meghasahgal@Meghas-Air.localdomain>
2023-01-15 15:34:15 -0800

    added images

--- react-app/src/components/SplashPage/index.js
+++ react-app/src/components/SplashPage/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
-import NavBar from "../NavBar/index";
 import "./SplashPage.css";
 import food from "./food.jpeg";
+import cook from "./cook.jpeg"
 
 const SplashPage = () => {
 	return (
 		<>
+            <body>
+                <div className="splash"></div>
+            </body>
 			<h1 className="primary-text">Rumble in Your Tummy?</h1>
 			<br></br>
 			<h3 className="tertiary-text">
-				Homecooked and FRESH food made to order just for you!{" "}
+				Homecooked and fresh food made to order just for you!{" "}
 			</h3>
 			<br></br>
 			<div>
@@ -32,7 +35,7 @@ const SplashPage = () => {
 				<a href="https://images.unsplash.com/photo-1484980972926-edee96e0960d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzJ8fGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60"></a>
 			</div>
 
-			<img id="splash-img" src={food} alt="splash-img" />
+			{/* <img id="splash-img" src={cook} alt="splash-img" /> */}
 		</>
 	);
 };
