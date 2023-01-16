a15382fe86e68edca4eac0b35cab87ee803e5121
Megha Sahgal <meghasahgal@Meghas-Air.localdomain>
2023-01-15 18:59:35 -0800

    styling for page not found

--- react-app/src/components/PageNotFound/index.jsx
+++ react-app/src/components/PageNotFound/index.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import "./PageNotFound.css"
+import { useHistory } from "react-router-dom";
+import homer from "../PageNotFound/homer.jpeg"
 
 const PageNotFound = () => {
+	const history = useHistory();
+	const routeChangeToHome = (e) => {
+		history.push("/");
+	};
+
 	return (
-		<div>
-			<h1>PAGE NOT FOUND</h1>
+		<div className="page-not-found">
+			<h1>Oops!</h1>
+			<p>We can't seem to find the page you're looking for.</p>
+			<img src={homer} alt="not found, sadly..." />
+
+			<button className="pnf-button" onClick={routeChangeToHome}>Go Back Home</button>
+
 		</div>
 	);
+
 };
 
 export default PageNotFound;
