a36c3aa7c2b3a1809abbaeadb3003acd447d106d
Alex Hiller <asonnyhiller@gmail.com>
2023-01-16 17:12:34 -0500

    profile page css in progress

--- react-app/src/components/Modals/AddShopForms/ModalAddShop.jsx
+++ react-app/src/components/Modals/AddShopForms/ModalAddShop.jsx
@@ -9,7 +9,6 @@ const ModalAddShop = ({ setIsOpen, userId }) => {
 	const dispatch = useDispatch();
 	const user = useSelector((state) => state.users[userId]);
 	// const history = useHistory();
-	console.log(user);
 
 	const [errors, setErrors] = useState([]);
 	const [shopName, setShopName] = useState("");
--- react-app/src/components/ProfilePage/UserInfo.jsx
+++ react-app/src/components/ProfilePage/UserInfo.jsx
@@ -1,13 +1,24 @@
+import './userprofileinfo.css'
+import ShopOwnerInfo from './ShopOwnerInfo';
+
 const UserInfo = ({user}) => {
     return (
-        <div>
-            <div>
-                <img 
+        <div className="userInfoBox">
+            <div className="userProfileImgBox">
+                <img
+                className="userProfileImg" 
                 src={user?.profileImg}
                 alt="Whoops! It looks like your link is broken."
                 />
             </div>
-            <div>{user?.username}</div>
+            <div className="usernameBox">
+                <h1 className="usernameDisplay">{user?.username}</h1>
+            {user.shopName ? (
+				<div className="shopownerInfo">
+					<ShopOwnerInfo user={user} />
+				</div>) : null}
+            </div>
+			
         </div>
     )
 }
