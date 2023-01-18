a3d9a4a597bd80809a08df0a3dbf0f3fedc18d3e
Alex Hiller <asonnyhiller@gmail.com>
2023-01-17 19:41:45 -0500

    fixed the review modal so review scores all have one decimal place

--- react-app/src/components/Modals/EditReview/ModalEditReview.jsx
+++ react-app/src/components/Modals/EditReview/ModalEditReview.jsx
@@ -78,15 +78,15 @@ const ModalEditReview = ({ setIsOpen, review }) => {
 											}
 										>
 											<option value="--">--</option>
-											<option value="1">1</option>
+											<option value="1.0">1.0</option>
 											<option value="1.5">1.5</option>
-											<option value="2">2</option>
+											<option value="2.0">2.0</option>
 											<option value="2.5">2.5</option>
-											<option value="3">3</option>
+											<option value="3.0">3.0</option>
 											<option value="3.5">3.5</option>
-											<option value="4">4</option>
+											<option value="4.0">4.0</option>
 											<option value="4.5">4.5</option>
-											<option value="5">5</option>
+											<option value="5.0">5.0</option>
 										</select>
 									</div>
 								</form>
